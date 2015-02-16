567270b7e215892984e0758c7af1ad4a8d9dc84c
cassiex <treefieldsun@gmail.com>
2015-02-16 10:28:01 -0800

    This is the first upload

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.ViewModels;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Login login)
+        {
+            // UserStore and UserManager manages data retreival.
+            UserStore<IdentityUser> userStore = new UserStore<IdentityUser>();
+            UserManager<IdentityUser> manager = new UserManager<IdentityUser>(userStore);
+            IdentityUser identityUser = manager.Find(login.UserName,
+                                                             login.Password);
+
+            if (ModelState.IsValid)
+            {
+                if (identityUser != null)
+                {
+                    IAuthenticationManager authenticationManager
+                                           = HttpContext.GetOwinContext().Authentication;
+                    authenticationManager
+                   .SignOut(DefaultAuthenticationTypes.ExternalCookie);
+
+                    var identity = new ClaimsIdentity(new[] {
+                                            new Claim(ClaimTypes.Name, login.UserName),
+                                        },
+                                        DefaultAuthenticationTypes.ApplicationCookie,
+                                        ClaimTypes.Name, ClaimTypes.Role);
+                    // SignIn() accepts ClaimsIdentity and issues logged in cookie. 
+                    authenticationManager.SignIn(new AuthenticationProperties
+                    {
+                        IsPersistent = false
+                    }, identity);
+                    return RedirectToAction("SecureArea", "Home");
+                }
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisteredUser newUser)
+        {
+            var userStore = new UserStore<IdentityUser>();
+            var manager = new UserManager<IdentityUser>(userStore);
+            var identityUser = new IdentityUser()
+            {
+                UserName = newUser.UserName,
+                Email = newUser.Email
+            };
+            IdentityResult result = manager.Create(identityUser, newUser.Password);
+
+            if (result.Succeeded)
+            {
+                var authenticationManager
+                                  = HttpContext.Request.GetOwinContext().Authentication;
+                var userIdentity = manager.CreateIdentity(identityUser,
+                                           DefaultAuthenticationTypes.ApplicationCookie);
+                authenticationManager.SignIn(new AuthenticationProperties() { },
+                                             userIdentity);
+            }
+            return View();
+        }
+        [Authorize]
+        public ActionResult SecureArea()
+        {
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            var ctx = Request.GetOwinContext();
+            var authenticationManager = ctx.Authentication;
+            authenticationManager.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public ActionResult AddRole()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddRole(AspNetRole role)
+        {
+            IdentityEntities context = new IdentityEntities();
+            context.AspNetRoles.Add(role);
+            context.SaveChanges();
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult AddUserToRole()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddUserToRole(string userName, string roleName)
+        {
+            IdentityEntities context = new IdentityEntities();
+            AspNetUser user = context.AspNetUsers
+                             .Where(u => u.UserName == userName).FirstOrDefault();
+            AspNetRole role = context.AspNetRoles
+                             .Where(r => r.Name == roleName).FirstOrDefault();
+
+            user.AspNetRoles.Add(role);
+            context.SaveChanges();
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        // To allow more than one role access use syntax like the following:
+        // [Authorize(Roles="Admin, Staff")]
+        public ActionResult AdminOnly()
+        {
+            return View();
+        }
+
+
+
+    }
+}
---
+++ WebApplication2/ViewModels/UserRole.cs
@@ -0,0 +1,5 @@
+﻿public class UserRole
+{
+    public string UserName { get; set; }
+    public string RoleName { get; set; }
+}
